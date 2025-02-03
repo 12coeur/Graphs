96c4f13386c7c8ee1985c17d2909e71dbfdd5854
asde coeur <12coeur@gmail.com>
2025-02-03 09:44:49 +0100

    Update script.js
    Rajout du magnétometer

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
-// Vérifiez si les capteurs sont disponibles
-if ('Accelerometer' in window && 'Gyroscope' in window) {
-  // Initialisez l'accéléromètre
-  const accelerometer = new Accelerometer({frequency: 60});
-  accelerometer.addEventListener('reading', () => {
-    document.getElementById('accelerometer-data').innerText = 
-      `Accéléromètre: 
-      X: ${accelerometer.x.toFixed(2)}, 
-      Y: ${accelerometer.y.toFixed(2)}, 
-      Z: ${accelerometer.z.toFixed(2)}`;
-  });
-  accelerometer.start();
-
-  // Initialisez le gyroscope
-  const gyroscope = new Gyroscope({frequency: 60});
-  gyroscope.addEventListener('reading', () => {
-    document.getElementById('gyroscope-data').innerText = 
-      `Gyroscope: 
-      X: ${gyroscope.x.toFixed(2)}, 
-      Y: ${gyroscope.y.toFixed(2)}, 
-      Z: ${gyroscope.z.toFixed(2)}`;
-  });
-  gyroscope.start();
+if ('Accelerometer' in window) {
+    let accelerometer = new Accelerometer({frequency: 60});
+    accelerometer.addEventListener('reading', () => {
+        document.getElementById('accelerometer-data').textContent =
+            `Accéléromètre: ${accelerometer.x}, ${accelerometer.y}, ${accelerometer.z}`;
+    });
+    accelerometer.start();
 } else {
-  document.getElementById('accelerometer-data').innerText = 'Accéléromètre non supporté';
-  document.getElementById('gyroscope-data').innerText = 'Gyroscope non supporté';
+    console.log('L\'API Accéléromètre n\'est pas disponible.');
+}
+
+if ('Gyroscope' in window) {
+    let gyroscope = new Gyroscope({frequency: 60});
+    gyroscope.addEventListener('reading', () => {
+        document.getElementById('gyroscope-data').textContent =
+            `Gyroscope: ${gyroscope.x}, ${gyroscope.y}, ${gyroscope.z}`;
+    });
+    gyroscope.start();
+} else {
+    console.log('L\'API Gyroscope n\'est pas disponible.');
+}
+
+if ('Magnetometer' in window) {
+    let gyroscope = new Magnetometer({frequency: 60});
+    magnetometer.addEventListener('reading', () => {
+        document.getElementById('magnetometer-data').textContent =
+            `Magnetometer: ${magnetometer.x}, ${magnetometer .y}, ${magnetometer .z}`;
+    });
+    magnetometer.start();
+} else {
+    console.log('L\'API Magnetometer n\'est pas disponible.');
 }
